7ca3b744c16bc87cae13d11b778babd4967e8746
binex <2540245653@qq.com>
2016-04-15 18:57:34 +0800

    add corrected item

--- readKBR.py
+++ readKBR.py
@@ -30,6 +30,12 @@ class KBRReader(readGRACE.GRACEReader):
             data_item_record_dict['K_B_SNR'] = int( data_item_record[13] )
             data_item_record_dict['Ka_B_SNR'] = int( data_item_record[14] )
             data_item_record_dict['qualflg'] = data_item_record[15]
+            data_item_record_dict['corrected_biased_range'] = data_item_record_dict['biased_range'] +\
+                    data_item_record_dict['lighttime_corr'] + data_item_record_dict['ant_centr_corr']
+            data_item_record_dict['corrected_range_rate']   = data_item_record_dict['range_rate']   +\
+                    data_item_record_dict['lighttime_rate'] + data_item_record_dict['ant_centr_rate']
+            data_item_record_dict['corrected_range_accl']   = data_item_record_dict['range_accl']   +\
+                    data_item_record_dict['lighttime_accl'] + data_item_record_dict['ant_centr_accl']
             self.grace_body.append( data_item_record_dict )
             return True
 
